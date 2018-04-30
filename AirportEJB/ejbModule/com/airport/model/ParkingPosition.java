351a768d69f157292446807324cedcf07446b035
Well5a <marcelfrederik@aol.de>
2018-04-30 13:45:21 +0200

    added ParkingPosition Entity

---
+++ AirportEJB/ejbModule/com/airport/model/ParkingPosition.java
@@ -0,0 +1,51 @@
+package com.airport.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class ParkingPosition {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String name;
+	
+	private boolean isLocked;
+	
+	private Airplane airplane;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	} 
+	
+	public boolean getIsLocked() {
+		return isLocked;
+	}
+	
+	public void setIsLocked(boolean isLocked) {
+		this.isLocked = isLocked;
+	}
+	
+	public Airplane getAirplane() {
+		return airplane;
+	}
+
+	public void setAirplane(Airplane airplane) {
+		this.airplane = airplane;
+	}
+}
