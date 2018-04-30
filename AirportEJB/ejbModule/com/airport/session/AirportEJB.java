1e87b2f83f5c4c6481ed89b15d4845f0ead762ac
david <david.schick@online.de>
2018-04-30 14:29:09 +0200

    updated airport EJB

--- AirportEJB/ejbModule/com/airport/session/AirportEJB.java
+++ AirportEJB/ejbModule/com/airport/session/AirportEJB.java
@@ -8,6 +8,9 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import com.airport.model.Airplane;
+import com.airport.model.ParkingPosition;
+import com.airport.model.Runway;
+
 
 @Stateless
 public class AirportEJB {
@@ -15,6 +18,7 @@ public class AirportEJB {
 	@PersistenceContext(unitName="airport")
 	private EntityManager entityManager;
 	
+	// -------------------------- Airplanes  ------------------------------------
 	public List<Airplane> getAirplanes() {
 		Query query = entityManager.createNamedQuery("airplane.findAll");
 		
@@ -22,8 +26,59 @@ public class AirportEJB {
 		List<Airplane> airplanes = query.getResultList();
 		return airplanes;
 	}
-	
+
 	public void store(Airplane airplane) {
 		entityManager.persist(airplane);
 	}
+	
+	// -------------------------- Runways ------------------------------------
+	@SuppressWarnings("unchecked")
+	public List<Runway> getRunways() {
+		Query query = entityManager.createNamedQuery("runway.findAll");
+		return query.getResultList();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Runway> getFreeRunways() {
+		Query query = entityManager.createNamedQuery("runway.findFree");
+		return query.getResultList();	
+	}
+	
+	
+	public void lockRunway(Runway runway) {
+		runway.setIsLocked(true);
+		entityManager.persist(runway);
+	}
+	
+	public void unlockRunway(Runway runway) {
+		runway.setIsLocked(false);
+		entityManager.persist(runway);
+	}
+	
+	
+	// -------------------------- Parking Positions ------------------------------------
+	@SuppressWarnings("unchecked")
+	public List<ParkingPosition> getParkingPositions() {
+		Query query = entityManager.createNamedQuery("parkingposition.findAll");
+		return query.getResultList();
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<ParkingPosition> getFreeParkingPositions() {
+		Query query = entityManager.createNamedQuery("parkingposition.findFree");
+		return query.getResultList();	
+	}
+	
+	public void lockParkingPosition(ParkingPosition parkingPosition) {
+		parkingPosition.setIsLocked(true);
+		entityManager.persist(parkingPosition);
+	}
+	
+	public void unlockParkingPosition(ParkingPosition parkingPosition) {
+		parkingPosition.setIsLocked(false);
+		entityManager.persist(parkingPosition);
+	}
+	
+	
 }
